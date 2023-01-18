1319cff185401dded05ae152bbc2ad168fec00e3
gabrielppd77 <gabrielppd77@outlook.com>
2023-01-18 19:15:33 -0300

    start layout in frontend and went make compoents

--- frontend/src/components/Habit.tsx
+++
@@ -1,8 +0,0 @@
-interface HabitProps {
-  completed: number;
-}
-
-export function Habit(props: HabitProps) {
-  const { completed } = props;
-  return <div className="bg-zinc-900 w-10">habit {completed}</div>;
-}
